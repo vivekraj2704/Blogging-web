7e03e006c9edf2254ffb75a412e38b6d7123a8c3
vivekraj2704 <rajvivek2704@gmail.com>
2024-09-24 13:10:21 +0530

    changed some function

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export const Appbar = () => {
     return <div className="border-b flex justify-between px-10 py-4">
         <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer">
-                Medium
+                The Write Way
         </Link>
         <div>
             <Link to={`/publish`}>
